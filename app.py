29c57cd7dcaa4f38e2a0117aedc7980493fd74ef
mkydh <michael.kevin@gmail.com>
2020-06-27 01:52:35 +0800

    moved instagram code

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
+import json
+from datetime import datetime
+
+import bs4
+import requests
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -10,6 +16,20 @@ from instagram_utils import *
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
+
+
+def get_insta_post(id: str):
+    api_url = f'https://instagram.com/tv/{id}'
+    soup = bs4.BeautifulSoup(requests.get(api_url).content, 'html.parser')
+
+    raw_text = soup.body.script.string.strip()
+
+    # Get rid of 'window._sharedData =' at front and ';' at back
+    start = raw_text.find('=')
+    end = raw_text.rfind(';')
+
+    js = json.loads(raw_text[start + 1:end])
+    return js['entry_data']['PostPage'][0]['graphql']['shortcode_media']
 
 
 @app.route('/')
